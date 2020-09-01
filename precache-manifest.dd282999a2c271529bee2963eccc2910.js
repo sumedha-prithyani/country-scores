9b22b30acc5cde746e5cb4fa40d42219d5612e46
sumedha <sumedha.prithyani@perficient.com>
2020-09-01 22:28:27 +0530

    notes app build deploy

---
+++ precache-manifest.dd282999a2c271529bee2963eccc2910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5b1ec9dd6ab6b2f97fb76bbac97d4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3058d72a1d5e4174ca88",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9847098d108f9c68d39f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3058d72a1d5e4174ca88",
+    "url": "/static/js/2.a3e86396.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a3e86396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9847098d108f9c68d39f",
+    "url": "/static/js/main.70561661.chunk.js"
+  },
+  {
+    "revision": "47d5b8819a0ae5ef1327",
+    "url": "/static/js/runtime-main.b81da802.js"
+  }
+]);
